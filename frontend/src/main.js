472535525165fe16b887e4aa1b402155892974b5
Jose Ortega Limon <ortlimjose@gmail.com>
2026-07-28 17:48:21 -0600

    Desarrollo del frontend de cocina
    Se agregaron los elementos visuales de la sección de cocina, además de agregar los estilos para cada uno y por último una función automática para refrescar las ordenes actuales.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,15 +2,19 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import { OhVueIcon, addIcons } from 'oh-vue-icons';
 import {
-  MdRestaurant, MdVisibility, MdVisibilityoff, MdWarning
+  MdRestaurant, MdVisibility, MdVisibilityoff, MdWarning,
+  MdRefresh, MdAccesstime, MdNotes, MdPerson, MdDone
 } from 'oh-vue-icons/icons/md';
+import { FaMoneyBillWaveAlt } from 'oh-vue-icons/icons/fa';
+import { BiCreditCard } from 'oh-vue-icons/icons/bi';
 import App from './App.vue';
 import router from './router';
 import './assets/main.css';
 
 // Agregar íconos
 addIcons(
-  MdRestaurant, MdVisibility, MdVisibilityoff, MdWarning
+  MdRestaurant, MdVisibility, MdVisibilityoff, MdWarning,
+  MdRefresh, MdAccesstime, MdNotes, MdPerson, MdDone, FaMoneyBillWaveAlt, BiCreditCard
 );
 
 const app = createApp(App);
